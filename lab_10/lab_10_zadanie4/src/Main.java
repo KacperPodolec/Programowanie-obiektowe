fb2503405daacee3f6b103750ec8497bdc3ff8c2
KacperPodolec <162919635+KacperPodolec@users.noreply.github.com>
2024-05-11 00:45:07 +0200

    Delete lab_10/lab_10_zadanie4 directory

--- lab_10/lab_10_zadanie4/src/Main.java
+++
@@ -1,5 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String[] produkty = {"Cukier", "Woda", "Pomidor", "Kawa", "Hebrata", "Masło", "Jajka", "Ciastka", "Sól", "Pieprz"};
-    }
-}
